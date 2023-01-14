683448a016c8aa751aa0f8515f4bc1f683161ae4
rnarcos <marcos@fernandescouto.com>
2023-01-14 11:12:41 -0300

    # Styles: * Creating GlobalStyles css reset;

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Router from './routes/_router';
+import { GlobalStyles } from './styles';
 
 createRoot(document.getElementById('root')!).render(
+  <>
+    <GlobalStyles />
     <Router />
+  </>,
 );
